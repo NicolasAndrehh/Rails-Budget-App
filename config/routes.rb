b5adb3f16f053d052c0ff48997b0cb5435c8ff9e
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-01 18:42:41 -0500

    Add devise authenticate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  root "records#index"
-  # Defines the root path route ("/")
+  devise_scope :user do
+    authenticated :user do
+      root to: 'categories#index', as: :authenticated_root
+    end
+    
+    unauthenticated do
+      root "home#index"
+    end
+  end
+  
+
   resources :records
   resources :categories
 end
