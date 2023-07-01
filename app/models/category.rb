42388776427bb268e82d5d562177dfe20a40e008
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-01 18:44:06 -0500

    Add validations to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+    belongs_to :user
+    has_many :record_categories, dependent: :destroy
+    has_many :records, through: :record_categories
+
+    validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+    validates :author_id, presence: true
 end
