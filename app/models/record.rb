b44eb08537f4c8aa8a09b40c6ac46b04ec1896c4
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-01 18:43:55 -0500

    Add validations to Record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,9 @@
 class Record < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :record_categories, dependent: :destroy
+  has_many :categories, through: :record_categories
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :author_id, presence: true
 end
